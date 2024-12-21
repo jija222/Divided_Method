b2c0533112546234df82cd78bb71c5150d43fa03
jija222 <bragihin@mail.ru>
2024-12-21 22:12:18 +0500

    first

---
+++ Divided_Method.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+const double exactNum = 3.23;
+
+double fX(double x) {
+	return (((3 * x) - (4 * log(x))) - 5);
+}
+
+double findX(double left, double right) {
+	return (left + right) / 2;
+}
+
+double func(double left, double right) {
+	if (round(left * 1000) / 1000 == exactNum || round(right * 1000) / 1000 == exactNum) {
+		cout << "end" << endl;
+		return 0;
+	}
+	double newX = findX(left, right);
+	cout << "New x is " << newX << endl;
+	if ((fX(left) * fX(newX)) < 0) {
+		return func(left, newX);
+	}
+	if ((fX(newX) * fX(right)) < 0) {
+		return func(newX, right);
+	}
+}
+
+int main() {
+	func(2, 4);
+	system("pause");
+	return 0;
+}
+
